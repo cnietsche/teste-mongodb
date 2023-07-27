909d1189c26be86513f4efe81ae1543217b70cd0
cnietsche <christian.nietsche@hotmail.com>
2023-07-26 21:25:40 -0300

    Arquivo root

--- teste-back-front/backend/app.js
+++ teste-back-front/backend/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.json("Root");
+});
+
 app.use(cors());
 
 app.use(express.json());
